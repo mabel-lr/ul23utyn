4307b84392b2e406c17f328ec0888c86b5c3ad2c
ul23utyn <belen.lojo@fau.de>
2021-11-11 16:35:00 +0100

    comments: new files

---
+++ ul23utyn/function.py
@@ -0,0 +1,49 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "id": "03f22c6a",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "import numpy as np\n",
+    "from ipywidgets import interact, fixed\n",
+    "from PIL import Image"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "bda6b57d",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "def imshow(X,resize=None):\n",
+    "    resized_img=X.resize(resize)\n",
+    "    return resized_img"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "myvenv",
+   "language": "python",
+   "name": "myvenv"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.9.4"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
